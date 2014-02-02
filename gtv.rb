5024c74adfa6998f618e5d497e350f94b490c54c
kristenmills <kristen@kristen-mills.com>
2014-02-01 22:26:50 -0500

    Uses new mycroft ruby gem

--- gtv.rb
+++ gtv.rb
@@ -16,65 +16,62 @@ class Gtv < Mycroft::Client
     super
   end
 
-  def connect
-    GTVRemote::open()
+  on 'connect' do
+    GTVRemote::open
   end
 
-  def on_data(data)
-    if data[:type] == 'MSG_BROADCAST'
-      # Ensure this broadcast is for us
-      unless (data[:data]["content"].has_key?("grammar") and data[:data]["content"]["grammar"] == "Google TV")
-        puts "Grammar isn't being matched oops"
-        return
-      end
-      # Get the app from the broadcast
-      app = data[:data]["content"]["tags"]["app"].downcase
-      
+  on 'MSG_BROADCAST' do |data|
+    unless (data["content"].has_key?("grammar") and data["content"]["grammar"] == "Google TV")
+      puts "Grammar isn't being matched oops"
+      return
+    end
+    # Get the app from the broadcast
+    app = data["content"]["tags"]["app"].downcase
+    
+    open_app(app)
+  end
+
+  on 'MSG_QUERY' do |data|
+    puts data["action"]
+    if data["action"].downcase == "app"
+      #TODO: Validate Query
+      app = data["data"]["app"].downcase
+      puts app
       open_app(app)
-
-    elsif data[:type] == 'MSG_QUERY'
-      puts data[:data]["action"]
-      if data[:data]["action"].downcase == "app"
-        #TODO: Validate Query
-        app = data[:data]["data"]["app"].downcase
-        puts app
-        open_app(app)
-      elsif data[:data]["action"].downcase == "url"
-        #TODO: Validate Query
-        url = data[:data]["data"]["url"]
-        if (not url.downcase.include?("http")) # Make sure URL has http:// in front
-          url = "http://" + url
-        end
-        GTVRemote::open_uri(url)
+    elsif data["action"].downcase == "url"
+      #TODO: Validate Query
+      url = data["data"]["url"]
+      if (not url.downcase.include?("http")) # Make sure URL has http:// in front
+        url = "http://" + url
       end
-    elsif data[:type] == 'APP_MANIFEST_OK'
-      # Do stuff
-      grammar = File.read("./grammar.xml")
-      data = {
-        "grammar"=> {
-          "name"=> "Google TV",
-          "xml"=> grammar
-        }
-      }
-      query("stt", "load_grammar", data)
-
-    elsif data[:type] == 'APP_DEPENDENCY'
-      # Do stuff
+      GTVRemote::open_uri(url)
     end
   end
 
-  def on_end
-    GTVRemote::close()
+  on 'APP_MANIFEST_OK' do |data|
+    grammar = File.read("./grammar.xml")
+    data = {
+      "grammar"=> {
+        "name"=> "Google TV",
+        "xml"=> grammar
+      }
+    }
+    query("stt", "load_grammar", data)
+  end
+
+  on 'end' do 
+    GTVRemote::close
+    query('stt', 'unload_grammar', {grammar: 'Google TV'})
   end
 
   def open_app(app)
-      # Make sure we recognize the command.
-      unless @gtv_packages.has_key?(app)
-        return
-      end
-      
-      # Send the appropriate intent to the GTV
-      GTVRemote::fling(@gtv_packages[app])
+    # Make sure we recognize the command.
+    unless @gtv_packages.has_key?(app)
+      return
+    end
+    
+    # Send the appropriate intent to the GTV
+    GTVRemote::fling(@gtv_packages[app])
   end
 end
 
