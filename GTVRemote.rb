83ab5595c8111af2a6ea40b6a70a523784608a8d
kristenmills <kristen@kristen-mills.com>
2014-03-31 22:19:30 -0400

    Should be hip and working and cool

--- GTVRemote.rb
+++ GTVRemote.rb
@@ -7,11 +7,11 @@ module GTVRemote
   @gtv_cert = File.read(File.join(File.dirname(__FILE__),"certs/cert.pem"))
   @host = "ssegoogletv.rit.edu"
 
-  def open
+  def pair
     @gtv = GoogleAnymote::TV.new(@gtv_cert, @host)
   end
 
-  def close
+  def unpair
     @gtv = nil
   end
 
@@ -22,5 +22,4 @@ module GTVRemote
   def send_keycode(k)
     @gtv.send_keycode(Code.values[k.to_sym]) unless Code.values[k.to_sym].nil?
   end
-
 end
--- gtv.rb
+++ gtv.rb
@@ -3,7 +3,7 @@ require 'mycroft'
 require 'json'
 require_relative "GTVRemote"
 
-class Gtv < Mycroft::Client
+class GTV < Mycroft::Client
   include GTVRemote
 
   def initialize(host, port)
@@ -12,8 +12,7 @@ class Gtv < Mycroft::Client
     @manifest = './app.json'
     @verified = false
     @sent_grammar = false
-    @gtv_packages = JSON.parse(File.read("package_names.json"))
-    GTVRemote::open
+    pair
     super
   end
 
@@ -22,35 +21,20 @@ class Gtv < Mycroft::Client
   end
 
   on 'MSG_QUERY' do |data|
-    puts data["action"]
-    if data["action"].downcase == "app"
-      #TODO: Validate Query
-      app = data["data"]["app"].downcase
-      puts app
-      open_app(app)
-    elsif data["action"].downcase == "url"
-      #TODO: Validate Query
-      url = data["data"]["url"]
-      if (not url.downcase.include?("http")) # Make sure URL has http:// in front
-        url = "http://" + url
-      end
-      GTVRemote::open_uri(url)
+    if data['action'] == 'keycode'
+      send_keycode(data['data']['keycode'])
+    elsif data['action'] == 'fling'
+      fling(data['data']['uri'])
     end
   end
 
   on 'CONNECTION_CLOSED' do
-    GTVRemote::close
+    unpair
   end
 
-  def open_app(app)
-    # Make sure we recognize the command.
-    unless @gtv_packages.has_key?(app)
-      return
-    end
-
-    # Send the appropriate intent to the GTV
-    GTVRemote::fling(@gtv_packages[app])
+  on 'ERROR' do |err|
+    puts err
   end
 end
 
-Mycroft.start(Gtv)
+Mycroft.start(GTV)
